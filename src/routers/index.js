21f6b3207a13c85ef70e2bb91f3be62d8e998438
beaverbee <n2313963123>
2022-07-11 11:04:18 +0800

    2022/7/11

---
+++ src/routers/index.js
@@ -0,0 +1,12 @@
+import Login from "../pages/login";
+
+const router=[
+    {
+        path:"login",
+        element:<Login></Login>
+    },
+    {
+        path:'backend'
+    }
+]
+export default router;
